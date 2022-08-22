fc966e394cb13beae09bec278a4b99e0a27a5583
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-22 21:59:51 +0100

    filters

---
+++ phase_one/11_array_operation/filter.js
@@ -0,0 +1,20 @@
+const checklength = (str) => {
+  if (str.length <= 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const numbers = [
+  '1763687364',
+  '4763687363',
+  '7867867862',
+  'aaaaaaaabbbbbbbcccccdddddddd'
+]
+
+const filterLongNumbers = (to_filter) => {
+  return to_filter.filter(checklength);
+}
+
+console.log(filterLongNumbers(numbers));
