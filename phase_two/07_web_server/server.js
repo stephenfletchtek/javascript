8f69d2e9482fd11b6231dc80a5ed96caf3cc6b06
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-26 21:24:48 +0100

    basic server

--- phase_two/07_web_server/server.js
+++ phase_two/07_web_server/server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let counter = 0
+
 app.get('/', (req, res) => {
   res.send('One is not amused')
+});
+
+app.post('/counter', (req, res) => {
+  counter++
+  res.send('Increment the counter')
+});
+
+app.get('/counter', (req, res) => {
+  res.send(`Counter is at: ${counter}`)
+});
+
+app.delete('/counter', (req, res) => {
+  counter = 0
+  res.send('Reset counter to zero')
 });
 
 console.log(`Server listening on localhost:${port}`);
