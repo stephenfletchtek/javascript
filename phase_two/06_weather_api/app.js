605c03ec87dac9980180ece92422e0adc91110ae
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-26 20:51:33 +0100

    simple web server

--- phase_two/06_weather_api/app.js
+++ phase_two/06_weather_api/app.js
@@ -3,17 +3,14 @@ const WeatherView = require('./weatherview')
 const Weather = require('./weather')
 
 const readline = require('readline');
-const { callbackify } = require('util');
-const rl = readline.createInterface(
-  process.stdin, process.stdout);
+const rl = readline.createInterface(process.stdin, process.stdout);
 
 const weather = new Weather(new WeatherApi, new WeatherView)
 
 const fetchweather = (answer) => {
   weather.fetch(answer);
-  setTimeout(() => { console.log(weather.displayWeather()) }, 1000);
+  setTimeout(() => { console.log(weather.displayWeather()); }, 1000);
 }
-
 
 rl.question('Enter a city to get the weather ', (answer) => {
   // fetch weather every 5 sec
